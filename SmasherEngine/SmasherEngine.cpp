59c4933d42a3359540ac5e982bab4addb4190595
Fleshy <Mr.MiracleMan111@gmail.com>
2025-07-23 15:51:02 -0400

    Add project files.

---
+++ SmasherEngine/SmasherEngine.cpp
@@ -0,0 +1,12 @@
+﻿// SmasherEngine.cpp : Defines the entry point for the application.
+//
+
+#include "SmasherEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
