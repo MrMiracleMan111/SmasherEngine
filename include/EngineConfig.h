13f1919e154a3318ca844ce34c9e05be5d454eca
Fleshy <Mr.MiracleMan111@gmail.com>
2025-07-26 04:38:19 -0400

    Added Game Engine Config

---
+++ include/EngineConfig.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+namespace Smasher {
+	struct EngineConfig {
+		const inline static int WINDOW_WIDTH = 640;
+		const inline static int WINDOW_HEIGHT = 420;
+		const inline static std::string TITLE = "Smasher Engine Game";
+	};
+}
