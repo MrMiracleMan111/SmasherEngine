c96c9f02787517a848d8742c50d592e33ed0bb09
Fleshy <Mr.MiracleMan111@gmail.com>
2025-07-30 16:01:35 -0400

    Added Transformable method access
    Added transformable methods from SFML to Transform2D Component.
    
    Changed return type from void to Transform2DComponent& so that operations can be chained for example:
    
    component.SetPosition(sf::Vector2f {1, 2}).SetScale(sf::Vector2f{3, 2});

--- Components/Transform2DComponent.h
+++ Components/Transform2DComponent.h
@@ -5,7 +5,65 @@
 #include "IComponent.h"
 
 namespace Smasher {
-	class SMASHER_API Transform2DComponent : public IComponent, public sf::Transformable {
+	class SMASHER_API Transform2DComponent : public IComponent, private sf::Transformable {
+	public:
+		Transform2DComponent& SetPosition(sf::Vector2f position) {
+			setPosition(position);
+			return *this;
+		}
 
+		Transform2DComponent& SetPosition(sf::Vector2f position) {
+			setPosition(position);
+			return *this;
+		}
+
+		Transform2DComponent& SetScale(sf::Vector2f factors) {
+			setScale(factors);
+			return *this;
+		}
+
+		Transform2DComponent& SetOrigin(sf::Vector2f origin) {
+			setOrigin(origin);
+			return *this;
+		}
+
+		sf::Vector2f GetPosition() const {
+			return getPosition();
+		}
+
+		Degrees GetRotation() const {
+			return (Degrees)(getRotation());
+		}
+
+		sf::Vector2f GetScale() const {
+			return getScale();
+		}
+
+		sf::Vector2f GetOrigin() const {
+			return getOrigin();
+		}
+
+		Transform2DComponent& Move(sf::Vector2f offset) {
+			move(offset);
+			return *this;
+		}
+
+		Transform2DComponent& Rotate(Degrees angle) {
+			rotate((float)angle);
+			return *this;
+		}
+
+		Transform2DComponent& Scale(sf::Vector2f factor) {
+			scale(factor);
+			return *this;
+		}
+
+		const sf::Transform & GetTransform() const {
+			return getTransform();
+		}
+
+		const sf::Transform & GetInverseTransform() const {
+			return getInverseTransform();
+		}
 	};
 }
