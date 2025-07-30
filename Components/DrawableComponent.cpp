0d48d92db9a0d09735b16f22def872c939e611db
Fleshy <Mr.MiracleMan111@gmail.com>
2025-07-30 16:20:03 -0400

    Switched to new GetTransform

--- Components/DrawableComponent.cpp
+++ Components/DrawableComponent.cpp
@@ -5,7 +5,7 @@ namespace Smasher {
 	void DrawableComponent::StaticRenderComponent(DrawableComponent& self, sf::RenderWindow& rWindow) {
 		if (self.m_TextureLoaded) {
 			Transform2DComponent& rTransform = self.GetEntity().GetComponent<Transform2DComponent>();
-			rWindow.draw(self.m_Sprite, rTransform.getTransform());
+			rWindow.draw(self.m_Sprite, rTransform.GetTransform());
 		}
 	}
 }
--- Components/TextComponent.cpp
+++ Components/TextComponent.cpp
@@ -5,7 +5,7 @@ namespace Smasher {
 	void TextComponent::StaticRenderComponent(TextComponent& self, sf::RenderWindow& rWindow) {
 		if (self.m_FontLoaded) {
 			Transform2DComponent& rTransform = self.GetEntity().GetComponent<Transform2DComponent>();
-			rWindow.draw(self.m_Text, rTransform.getTransform());
+			rWindow.draw(self.m_Text, rTransform.GetTransform());
 		}
 	}
 }
