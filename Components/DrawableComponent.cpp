dedac864fa2ac5cfb19c52f2d5fd8c79f214cdfd
Fleshy <Mr.MiracleMan111@gmail.com>
2025-08-04 12:55:45 -0400

    Removed unused methods

--- Components/DrawableComponent.cpp
+++ Components/DrawableComponent.cpp
@@ -32,34 +32,6 @@ namespace Smasher {
     void DrawableComponent::SetEntity(Entity& pEntity) {
         IComponent::SetEntity(pEntity);
         assert(GetEntity().HasComponent<Transform2DComponent>());
-        m_TransformRef = &GetEntity().GetComponent<Transform2DComponent>().GetTransform();
-    }
-
-    void DrawableComponent::applyCurrentView(sf::RenderTarget& target) const
-    {
-        // Set the viewport
-        sf::IntRect viewport = getViewport(target, target.getView());
-        int top = target.getSize().y - (viewport.top + viewport.height);
-        (glViewport(viewport.left, top, viewport.width, viewport.height));
-
-        // Set the projection matrix
-        (glMatrixMode(GL_PROJECTION));
-        (glLoadMatrixf(target.getView().getTransform().getMatrix()));
-
-        // Go back to model-view mode
-        (glMatrixMode(GL_MODELVIEW));
-    }
-
-    sf::IntRect DrawableComponent::getViewport(sf::RenderTarget& target, const sf::View& view) const
-    {
-        float width = static_cast<float>(target.getSize().x);
-        float height = static_cast<float>(target.getSize().y);
-        const sf::FloatRect& viewport = view.getViewport();
-
-        return sf::IntRect(
-            static_cast<int>(0.5f + width * viewport.left),
-            static_cast<int>(0.5f + height * viewport.top),
-            static_cast<int>(width * viewport.width),
-            static_cast<int>(height * viewport.height));
+        m_TransformPtr = &GetEntity().GetComponent<Transform2DComponent>().GetTransform();
     }
 }
