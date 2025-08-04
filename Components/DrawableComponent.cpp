44f5503961e091891b3328c99c8058c5080138b1
Fleshy <Mr.MiracleMan111@gmail.com>
2025-08-04 16:01:27 -0400

    Finished clipping transform calculation

--- Components/DrawableComponent.cpp
+++ Components/DrawableComponent.cpp
@@ -66,11 +66,19 @@ namespace Smasher {
         m_ClipTransform = sf::Transform::Identity;
         sf::Texture& pTexture = m_TextureResource->GetTexture();
         sf::Vector2f dimensions = sf::Vector2f(pTexture.getSize().x, pTexture.getSize().y);
-        float left = m_ClipRect.left + std::copysign(1.0f, m_ClipRect.width) * -0.5f;
-        float top = m_ClipRect.top + std::copysign(1.0f, m_ClipRect.height) * -0.5f;
+        
+        float initialX = 0.5f * m_ClipRect.width;
+        float initialY = 0.5f * m_ClipRect.height;
 
-        m_ClipTransform.scale(m_ClipRect.width / dimensions.x, m_ClipRect.height / dimensions.y);
-        m_ClipTransform.translate(left / dimensions.x, top / dimensions.y);
+        float left = m_ClipRect.left - initialX; // +std::copysign(1.0f, m_ClipRect.width) * -0.5f;
+        float top = m_ClipRect.top - initialY; //+std::copysign(1.0f, m_ClipRect.height) * -0.5f;
+
+
+        m_ClipTransform
+            .translate(initialX / dimensions.x, initialY / dimensions.y) // Center the clip at 0,0
+            .rotate((float)m_ClipRotation)
+            .scale(m_ClipRect.width / dimensions.x, m_ClipRect.height / dimensions.y)
+            .translate(left / dimensions.x, top / dimensions.y);
 
         m_ClipChanged = false;
         return m_ClipTransform;
