9b0b01edef2c846caf94ac2bda1e4f0447d7b671
Fleshy <Mr.MiracleMan111@gmail.com>
2025-08-04 16:47:53 -0400

    Added Stress Test Files
    Added stress test target to CMake and files (not yet implemented)

---
+++ stress_test/assets/shaders/basic_texture_shader/basic_texture_vertex_shader.glsl
@@ -0,0 +1,29 @@
+#version 330 core
+precision highp int;
+layout(location = 0) in vec2 aPos; // the position variable has attribute position 0
+layout(location = 1) in vec2 aTexCoord; // the color variable has attribute position 1
+layout(location = 2) in mat4 model;
+layout(location = 6) in mat3 texMatrix;
+layout(location = 9) in uint aColorCode; // the color variable has attribute position 1
+
+out vec4 vertexColor;
+out vec2 texCoord;
+
+uniform mat4 ViewProjectionMatrix;
+uniform ivec2 textureSize;
+
+void main()
+{
+    vertexColor = vec4(
+        float((aColorCode >> 0) & 255u) / 255.0f,
+        float((aColorCode >> 8) & 255u) / 255.0f,
+        float((aColorCode >> 16) & 255u) / 255.0f,
+        float((aColorCode >> 24) & 255u) / 255.0f
+    );
+
+    // transform the vertex position
+    gl_Position = ViewProjectionMatrix * model * vec4(aPos, 0.0, 1.0);
+
+    vec3 texTmp = texMatrix * vec3(aTexCoord, 1.0);
+    texCoord = texTmp.xy;
+}
---
+++ stress_test/main.cpp
@@ -0,0 +1,87 @@
+#include <SFML/Window.hpp>
+#include "Core.h"
+#include "Manifest.h"
+#include "ExampleResourcesGameState.h"
+#include "Components/Transform2DComponent.h"
+#include "Components/TextComponent.h"
+#include "ComponentManagers/DrawableComponentManager.h"
+#include "Components/DrawableComponent.h"
+#include "Entity.h"
+
+using namespace Smasher;
+int main() {
+	Smasher::Engine engine(640, 420);
+
+	static_assert(HasRenderCapability<DrawableComponentManager>, "Failed");
+
+	engine.GetResourceManager().SetResourceDirectory(Resources::Metadata::RESOURCES_DIRECTORY);
+	ExampleResourcesGameState& state = engine.AddState<ExampleResourcesGameState>();
+
+	std::shared_ptr<ShaderResource> shader = engine.GetResourceManager().GetOrLoadResource<Resources::Shaders::basic_texture_shader, ShaderResource>();
+	sf::Vector2f windowSize = sf::Vector2f(engine.GetWindow().getSize().x, engine.GetWindow().getSize().y);
+	sf::Glsl::Mat4 viewProjectionMatrix = sf::Glsl::Mat4(engine.GetWindow().getView().getTransform().getMatrix());
+	shader->GetShader().setUniform("windowSize", windowSize);
+	shader->GetShader().setUniform("ViewProjectionMatrix", viewProjectionMatrix);
+
+	// TODO: FIND A BETTER SOLUTION
+// I shouldn't have to worry about loading DrawableComponentManager after drawable component
+	DrawableComponentManager& rCompManager = static_cast<DrawableComponentManager&>(state.GetComponentManager<DrawableComponent>());
+	rCompManager.SetShaderResource(shader);
+
+	Entity& image = state.AddEntity<Smasher::Entity>();
+	image.AddComponent<Transform2DComponent>()
+		.SetPosition(sf::Vector2f(500.0f, 300.0f))
+		.SetScale(sf::Vector2f(400.0f, 400.0f));
+	image.AddComponent<DrawableComponent>()
+		.SetShader(shader)
+		.SetTextureAsset<Smasher::Resources::Textures::small_art>()
+		//.SetClipRect(sf::IntRect{ 0, 0, 64, 64 })
+		.SetClipRotation(Smasher::Degrees{ 30 })
+		.SetDepth(0.2f)
+		.PushToGPU(); // Render in reverse with i=0 being on top
+
+	// Create 10 entites
+	// Half will have a non-alpha image
+	// Half will have image with alpha pixels
+	const std::size_t numEntities = 10;
+	for (int i = 0; i < numEntities; ++i) {
+		float depth = (float)(rand() % 100) / 100.0f;
+
+		int separation = 20;
+		int offset = 0;
+		Entity& image = state.AddEntity<Smasher::Entity>();
+		image.AddComponent<Transform2DComponent>()
+			.SetPosition(sf::Vector2f((float)(i * separation + offset), (float)(i * separation + offset)))
+			.SetScale(sf::Vector2f(100.0f, 100.0f));
+
+		image.AddComponent<DrawableComponent>()
+			.SetShader(shader)
+			.SetClipRect(sf::IntRect{ 0, 0, 30, 20 })
+			.SetDepth(depth);
+
+		if (i % 2 == 0) {
+			image.GetComponent<DrawableComponent>()
+				.SetTextureAsset<Smasher::Resources::Textures::alpha_test>()
+				.PushToGPU();
+		}
+		else {
+			image.GetComponent<DrawableComponent>()
+				.SetTextureAsset<Smasher::Resources::Textures::small_art>()
+				.PushToGPU();
+		}
+
+	}
+
+	Entity& message = state.AddEntity<Smasher::Entity>();
+	message.AddComponent<Transform2DComponent>();
+	message.AddComponent<TextComponent>()
+		.SetString("Hello World")
+		.UseDefaults()
+		.SetFontAsset<Smasher::Resources::Fonts::arial>();
+
+
+
+	state.Activate();
+	engine.Run();
+	return 0;
+}
