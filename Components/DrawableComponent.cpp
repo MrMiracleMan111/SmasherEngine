7bb9a373ceaf44ece61978ff4474db6a6bd4437c
Fleshy <Mr.MiracleMan111@gmail.com>
2025-08-04 13:25:09 -0400

    Moved Setter definitions to CPP file

--- Components/DrawableComponent.cpp
+++ Components/DrawableComponent.cpp
@@ -17,21 +17,51 @@ namespace Smasher {
         return *this;
     }
 
-    sf::Transform DrawableComponent::GetClipTransform()
+    const sf::Transform& DrawableComponent::GetClipTransform()
     {
+        assert(m_TextureLoaded);
+        if (!m_ClipChanged) {
+            return m_ClipTransform;
+        }
+
+        m_ClipTransform = sf::Transform::Identity;
+        sf::Texture& pTexture = m_TextureResource->GetTexture();
+        sf::Vector2f dimensions = sf::Vector2f(pTexture.getSize().x, pTexture.getSize().y);
         sf::Transform transform;
         float left = m_ClipRect.left + std::copysign(1.0f, m_ClipRect.width) * -0.5f;
         float top = m_ClipRect.top + std::copysign(1.0f, m_ClipRect.height) * -0.5f;
 
-        transform.scale(m_ClipRect.width , m_ClipRect.height);
-        transform.translate(left, top);
+        transform.scale(m_ClipRect.width / dimensions.x, m_ClipRect.height / dimensions.y);
+        transform.translate(left / dimensions.x, top / dimensions.y);
 
-        return transform;
+        m_ClipChanged = false;
+        return m_ClipTransform;
     }
 
     void DrawableComponent::SetEntity(Entity& pEntity) {
         IComponent::SetEntity(pEntity);
         assert(GetEntity().HasComponent<Transform2DComponent>());
         m_TransformPtr = &GetEntity().GetComponent<Transform2DComponent>().GetTransform();
+    }
+
+    DrawableComponent& DrawableComponent::SetDepth(float depth) {
+        m_Depth = depth;
+        return *this;
+    }
+
+    DrawableComponent& DrawableComponent::SetColor(sf::Color color) {
+        m_Color = color;
+        return *this;
+    }
+
+    DrawableComponent& DrawableComponent::SetShader(std::shared_ptr<ShaderResource> pShader) {
+        m_ShaderResource = pShader;
+        return *this;
+    }
+
+    DrawableComponent& DrawableComponent::SetClipRect(sf::IntRect clipRect) {
+        m_ClipChanged = true;
+        m_ClipRect = clipRect;
+        return *this;
     }
 }
--- Components/DrawableComponent.h
+++ Components/DrawableComponent.h
@@ -40,32 +40,20 @@ namespace Smasher {
 		virtual void SetEntity(Entity& pEntity);
 
 		// Depth bassed to depth buffer shader [0 - 1] with [0] being top and [1] being bottom
-		DrawableComponent& SetDepth(float depth) {
-			m_Depth = depth;
-			return *this;
-		}
+		DrawableComponent& SetDepth(float depth);
 
-		DrawableComponent& SetColor(sf::Color color) {
-			m_Color = color;
-			return *this;
-		}
+		DrawableComponent& SetColor(sf::Color color);
 
-		DrawableComponent& SetShader(std::shared_ptr<ShaderResource> pShader) {
-			m_ShaderResource = pShader;
-			return *this;
-		}
+		DrawableComponent& SetShader(std::shared_ptr<ShaderResource> pShader);
 
-		DrawableComponent& SetClipRect(sf::IntRect clipRect) {
-			m_ClipRect = clipRect;
-			return *this;
-		}
+		DrawableComponent& SetClipRect(sf::IntRect clipRect);
 
 		// Pushes transform to RenderBatch for rendering
 		// This MUST be called after changing Transform
 		// Try to call this as infrequently as possible
 		DrawableComponent& PushToGPU();
 
-		const sf::Transform& GetTransformPtr() {
+		const sf::Transform& GetTransformPtr() const {
 			assert(m_TransformPtr != nullptr);
 			return *m_TransformPtr;
 		}
@@ -74,7 +62,7 @@ namespace Smasher {
 
 		sf::Color GetColor() const { return m_Color; }
 
-		sf::Transform GetClipTransform();
+		const sf::Transform& GetClipTransform();
 
 	protected:
 		BatchContext m_OpaqueBatchContext;
@@ -84,8 +72,10 @@ namespace Smasher {
 	private:
 		sf::IntRect m_ClipRect{0, 0, 100, 100};
 		sf::Color m_Color = sf::Color::White;
+		sf::Transform m_ClipTransform;
 		sf::Transform const* m_TransformPtr = nullptr; // Cache it's location
 		bool m_TextureLoaded = false;
+		bool m_ClipChanged = false;
 		float m_Depth = 0.0f;
 	};
 }
