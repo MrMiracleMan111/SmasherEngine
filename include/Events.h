1a90c2a0e0f67a33c5299470c08ee9624fb4850a
Fleshy <Mr.MiracleMan111@gmail.com>
2025-08-07 13:38:43 -0400

    Moved Events into Smasher::Events namespace

--- include/Events.h
+++ include/Events.h
@@ -22,68 +22,69 @@ namespace Smasher {
 		Event(SMASHER_TIMESTAMP timestamp) : Timestamp(timestamp) {};
 	};
 
-	
-	struct SMASHER_API DummyEvent : public Event {
-		const char *Message;
-		SMASHER_EVENT_TYPE(EventType::DummyEvent)
-		DummyEvent() = delete;
-		DummyEvent(SMASHER_TIMESTAMP timestamp, const char *message) :
-			Event(timestamp), Message(message) {
+	namespace Events {	
+		struct SMASHER_API DummyEvent : public Event {
+			const char *Message;
+			SMASHER_EVENT_TYPE(EventType::DummyEvent)
+			DummyEvent() = delete;
+			DummyEvent(SMASHER_TIMESTAMP timestamp, const char *message) :
+				Event(timestamp), Message(message) {
+			};
 		};
-	};
 
-	struct SMASHER_API DummyEventExtra : public Event {
-		const char* Message;
-		SMASHER_EVENT_TYPE(EventType::DummyEventExtra)
-		DummyEventExtra() = delete;
-		DummyEventExtra(SMASHER_TIMESTAMP timestamp, const char* message) :
-			Event(timestamp), Message(message) {
+		struct SMASHER_API DummyEventExtra : public Event {
+			const char* Message;
+			SMASHER_EVENT_TYPE(EventType::DummyEventExtra)
+			DummyEventExtra() = delete;
+			DummyEventExtra(SMASHER_TIMESTAMP timestamp, const char* message) :
+				Event(timestamp), Message(message) {
+			};
 		};
-	};
 
-	struct SMASHER_API KeyboardEvent : public Event {
-		Smasher::Keyboard::KeyboardEventType Type;
-		sf::Keyboard::Key KeyCode;
-		SMASHER_EVENT_TYPE(EventType::KeyboardEvent)
-		KeyboardEvent() = delete;
-		KeyboardEvent(SMASHER_TIMESTAMP timestamp, Keyboard::KeyboardEventType type, sf::Keyboard::Key keycode) :
-			Event(timestamp), Type(type), KeyCode(keycode) {};
-	};
+		struct SMASHER_API KeyboardEvent : public Event {
+			Smasher::Keyboard::KeyboardEventType Type;
+			sf::Keyboard::Key KeyCode;
+			SMASHER_EVENT_TYPE(EventType::KeyboardEvent)
+			KeyboardEvent() = delete;
+			KeyboardEvent(SMASHER_TIMESTAMP timestamp, Keyboard::KeyboardEventType type, sf::Keyboard::Key keycode) :
+				Event(timestamp), Type(type), KeyCode(keycode) {};
+		};
 
-	// Intermediate class to represent mouse events
-	struct MouseEvent : public Event {
-		Mouse::MouseEventType Type;
-		EventType GetEventType() = 0;
-		~MouseEvent() {};
-	protected:
-		MouseEvent() = delete;
-		MouseEvent(SMASHER_TIMESTAMP timestamp, Mouse::MouseEventType type) :
-			Event(timestamp), Type(type) {};
-	};
+		// Intermediate class to represent mouse events
+		struct MouseEvent : public Event {
+			Mouse::MouseEventType Type;
+			EventType GetEventType() = 0;
+			~MouseEvent() {};
+		protected:
+			MouseEvent() = delete;
+			MouseEvent(SMASHER_TIMESTAMP timestamp, Mouse::MouseEventType type) :
+				Event(timestamp), Type(type) {};
+		};
 
-	struct SMASHER_API MouseButtonEvent : public MouseEvent {
-		sf::Mouse::Button ButtonCode;
-		SMASHER_EVENT_TYPE(EventType::MouseButtonEvent)
-		MouseButtonEvent(SMASHER_TIMESTAMP timestamp, Mouse::MouseEventType type, sf::Mouse::Button buttoncode) :
-			MouseEvent(timestamp, type), ButtonCode(buttoncode) {};
-	};
+		struct SMASHER_API MouseButtonEvent : public MouseEvent {
+			sf::Mouse::Button ButtonCode;
+			SMASHER_EVENT_TYPE(EventType::MouseButtonEvent)
+			MouseButtonEvent(SMASHER_TIMESTAMP timestamp, Mouse::MouseEventType type, sf::Mouse::Button buttoncode) :
+				MouseEvent(timestamp, type), ButtonCode(buttoncode) {};
+		};
 
-	struct SMASHER_API MouseScrollWheelEvent : public MouseEvent {
-		int Delta;
-		SMASHER_EVENT_TYPE(EventType::MouseScrollWheelEvent)
-		MouseScrollWheelEvent(SMASHER_TIMESTAMP timestamp, Mouse::MouseEventType type, int delta) :
-			MouseEvent(timestamp, type), Delta(delta) {};
-	};
+		struct SMASHER_API MouseScrollWheelEvent : public MouseEvent {
+			int Delta;
+			SMASHER_EVENT_TYPE(EventType::MouseScrollWheelEvent)
+			MouseScrollWheelEvent(SMASHER_TIMESTAMP timestamp, Mouse::MouseEventType type, int delta) :
+				MouseEvent(timestamp, type), Delta(delta) {};
+		};
 
-	struct SMASHER_API MouseMoveEvent : public MouseEvent {
-		sf::Vector2f Delta;
-		SMASHER_EVENT_TYPE(EventType::MouseMoveEvent)
-		MouseMoveEvent(SMASHER_TIMESTAMP timestamp, Mouse::MouseEventType type, sf::Vector2f delta) :
-			MouseEvent(timestamp, type), Delta(delta) {};
-	};
+		struct SMASHER_API MouseMoveEvent : public MouseEvent {
+			sf::Vector2f Delta;
+			SMASHER_EVENT_TYPE(EventType::MouseMoveEvent)
+			MouseMoveEvent(SMASHER_TIMESTAMP timestamp, Mouse::MouseEventType type, sf::Vector2f delta) :
+				MouseEvent(timestamp, type), Delta(delta) {};
+		};
 
-	struct SMASHER_API WindowCloseEvent : public Event {
-		SMASHER_EVENT_TYPE(EventType::WindowCloseEvent)
-		WindowCloseEvent(SMASHER_TIMESTAMP timestamp) : Event(timestamp) {}
-	};
+		struct SMASHER_API WindowCloseEvent : public Event {
+			SMASHER_EVENT_TYPE(EventType::WindowCloseEvent)
+			WindowCloseEvent(SMASHER_TIMESTAMP timestamp) : Event(timestamp) {}
+		};
+	}
 }
