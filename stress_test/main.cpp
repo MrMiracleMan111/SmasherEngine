6d646e06f1eb426f69a89082bbc8fd7c8eb415dd
Fleshy <Mr.MiracleMan111@gmail.com>
2025-08-11 20:59:01 -0400

    Reduced default number of balls to make camera movement visible

--- stress_test/main.cpp
+++ stress_test/main.cpp
@@ -14,7 +14,7 @@
 using namespace Smasher;
 int main(int argc, char **argv) {
 	Smasher::Engine engine(640, 420);
-	std::size_t numEntities = 100000;
+	std::size_t numEntities = 20;// 100000;
 
 	if (argc >= 2) {
 		numEntities = std::size_t{ std::stoull(argv[1]) };
