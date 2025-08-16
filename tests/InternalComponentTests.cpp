f68ed3c24213dce918333b089689d1f904f0a352
Fleshy <Mr.MiracleMan111@gmail.com>
2025-08-16 13:12:45 -0400

    Switched to using fixtures for InteralComponentTests

--- tests/InternalComponentTests.cpp
+++ tests/InternalComponentTests.cpp
@@ -7,9 +7,29 @@ public:
 	DummyGameState(Smasher::Engine& engine) : Smasher::GameState(engine) {}
 };
 
-TEST(TransformComponentTests, AddTransformComponent) {
-	Smasher::Engine engine(640, 420);
-	DummyGameState& state = engine.AddState<DummyGameState>();
-	Smasher::Entity& entity = state.AddEntity<Smasher::Entity>();
-	EXPECT_NO_THROW({ state.GetEntity(entity.GetUUID()); });
+class EngineSetupFixture : public ::testing::Test {
+	void SetUp() override;
+	void TearDown() override;
+protected:
+	std::unique_ptr<Smasher::Engine> pEngine;
+	Smasher::GameState* pState = nullptr;
+};
+
+void EngineSetupFixture::SetUp() {
+	pEngine = std::make_unique<Smasher::Engine>(640, 420);
+	pState = &pEngine->AddState<DummyGameState>();
+}
+
+void EngineSetupFixture::TearDown() {
+	pEngine.reset();
+}
+
+class Transform2DComponentFixture : public EngineSetupFixture {};
+
+// Transform2D Tests
+TEST_F(Transform2DComponentFixture, AddTransformComponent) {
+	Smasher::Entity& entity = pState->AddEntity<Smasher::Entity>();
+	EXPECT_NO_THROW({ pState->GetEntity(entity.GetUUID()); });
+}
+
 }
