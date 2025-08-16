2f9f62d65a2e00dbf9fe5c39da56fe473fd74258
Fleshy <Mr.MiracleMan111@gmail.com>
2025-08-16 13:32:17 -0400

    Using entity pointer for fixture tests

--- tests/InternalComponentTests.cpp
+++ tests/InternalComponentTests.cpp
@@ -14,11 +14,13 @@ class EngineSetupFixture : public ::testing::Test {
 protected:
 	std::unique_ptr<Smasher::Engine> pEngine;
 	Smasher::GameState* pState = nullptr;
+	Smasher::Entity* pEntity = nullptr;
 };
 
 void EngineSetupFixture::SetUp() {
 	pEngine = std::make_unique<Smasher::Engine>(640, 420);
 	pState = &pEngine->AddState<DummyGameState>();
+	pEntity = &pState->AddEntity<Smasher::Entity>();
 }
 
 void EngineSetupFixture::TearDown() {
@@ -30,38 +32,35 @@ class CameraComponentFixture : public EngineSetupFixture {};
 
 // Transform2D Tests
 TEST_F(Transform2DComponentFixture, AddTransformComponent) {
-	Smasher::Entity& entity = pState->AddEntity<Smasher::Entity>();
-	EXPECT_NO_THROW({ pState->GetEntity(entity.GetUUID()); });
+	EXPECT_NO_THROW({ pState->GetEntity(pEntity->GetUUID()); });
 }
 
 // Camera Component Tests
 TEST_F(CameraComponentFixture, RenderTargetNotSet) {
-	Smasher::Entity& entity = pState->AddEntity<Smasher::Entity>();
-	EXPECT_NO_THROW({ pState->GetEntity(entity.GetUUID()); });
+	EXPECT_NO_THROW({ pState->GetEntity(pEntity->GetUUID()); });
 
-	entity.AddComponent<Smasher::CameraComponent>();
+	pEntity->AddComponent<Smasher::CameraComponent>();
 
 	EXPECT_THROW({
-		entity.GetComponent<Smasher::CameraComponent>().ApplyToTarget();
+		pEntity->GetComponent<Smasher::CameraComponent>().ApplyToTarget();
 	}, Smasher::Exceptions::CameraTargetNotSet);
 }
 
 TEST_F(CameraComponentFixture, ApplyRenderTarget) {
-	Smasher::Entity& entity = pState->AddEntity<Smasher::Entity>();
 
-	entity.AddComponent<Smasher::CameraComponent>();
+	pEntity->AddComponent<Smasher::CameraComponent>();
 
-	EXPECT_NE(&entity.GetComponent<Smasher::CameraComponent>().GetView(), &pEngine->GetWindow().getView());
+	EXPECT_NE(&pEntity->GetComponent<Smasher::CameraComponent>().GetView(), &pEngine->GetWindow().getView());
 
 	EXPECT_NO_THROW({
-		entity.GetComponent<Smasher::CameraComponent>()
+		pEntity->GetComponent<Smasher::CameraComponent>()
 				.SetPosition(sf::Vector2f(10.0f, 15.0f))
 				.SetRotation(Smasher::Degrees { 35 })
 				.SetSize(sf::Vector2f(104.0f, 105.0f))
 				.ApplyToTarget(pEngine->GetWindow());
 	});
 
-	EXPECT_EQ(entity.GetComponent<Smasher::CameraComponent>().GetPosition(), pEngine->GetWindow().getView().getCenter());
-	EXPECT_EQ(entity.GetComponent<Smasher::CameraComponent>().GetRotation(), pEngine->GetWindow().getView().getRotation());
-	EXPECT_EQ(entity.GetComponent<Smasher::CameraComponent>().GetSize(), pEngine->GetWindow().getView().getSize());
+	EXPECT_EQ(pEntity->GetComponent<Smasher::CameraComponent>().GetPosition(), pEngine->GetWindow().getView().getCenter());
+	EXPECT_EQ(pEntity->GetComponent<Smasher::CameraComponent>().GetRotation(), pEngine->GetWindow().getView().getRotation());
+	EXPECT_EQ(pEntity->GetComponent<Smasher::CameraComponent>().GetSize(), pEngine->GetWindow().getView().getSize());
 }
