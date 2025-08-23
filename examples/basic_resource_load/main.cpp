c56240fab5c9868addc3361dcdac7e39b8376b0e
Fleshy <Mr.MiracleMan111@gmail.com>
2025-08-23 11:19:05 -0400

    Remoced "using namespace Smasher;"

--- examples/basic_resource_load/main.cpp
+++ examples/basic_resource_load/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <SFML/Window.hpp>
 #include "Core.h"
 #include "Manifest.h"
@@ -6,18 +7,16 @@
 #include "Components/TextComponent.h"
 #include "ComponentManagers/DrawableComponentManager.h"
 #include "Components/DrawableComponent.h"
-#include "Entity.h"
 
-using namespace Smasher;
 int main() {
 	Smasher::Engine engine(640, 420);
 
-	static_assert(HasRenderCapability<DrawableComponentManager>, "Failed");
+	static_assert(Smasher::HasRenderCapability<Smasher::DrawableComponentManager>, "Failed");
 
-	engine.GetResourceManager().SetResourceDirectory(Resources::Metadata::RESOURCES_DIRECTORY);
+	engine.GetResourceManager().SetResourceDirectory(Smasher::Resources::Metadata::RESOURCES_DIRECTORY);
 	ExampleResourcesGameState& state = engine.AddState<ExampleResourcesGameState>();
 
-	std::shared_ptr<ShaderResource> shader = engine.GetResourceManager().GetOrLoadResource<Resources::Shaders::basic_texture_shader, ShaderResource>();
+	std::shared_ptr<Smasher::ShaderResource> shader = engine.GetResourceManager().GetOrLoadResource<Smasher::Resources::Shaders::basic_texture_shader, Smasher::ShaderResource>();
 	sf::Vector2f windowSize = sf::Vector2f(engine.GetWindow().getSize().x , engine.GetWindow().getSize().y);
 	sf::Glsl::Mat4 viewProjectionMatrix = sf::Glsl::Mat4(engine.GetWindow().getView().getTransform().getMatrix());
 	shader->GetShader().setUniform("windowSize", windowSize);
@@ -25,12 +24,12 @@ int main() {
 	
 
 	// TODO: FIND A BETTER SOLUTION
-// I shouldn't have to worry about loading DrawableComponentManager after drawable component
-	DrawableComponentManager& rCompManager = static_cast<DrawableComponentManager&>(state.GetComponentManager<DrawableComponent>());
+	// I shouldn't have to worry about loading DrawableComponentManager after drawable component
+	Smasher::DrawableComponentManager& rCompManager = static_cast<Smasher::DrawableComponentManager&>(state.GetComponentManager<Smasher::DrawableComponent>());
 	rCompManager.SetShaderResource(shader);
 
-	Entity& image = state.AddEntity<Smasher::Entity>();
-	image.AddComponent<DrawableComponent>()
+	Smasher::Entity& image = state.AddEntity<Smasher::Entity>();
+	image.AddComponent<Smasher::DrawableComponent>()
 		.SetPosition(sf::Vector2f(500.0f, 300.0f))
 		.SetScale(sf::Vector2f(400.0f, 400.0f))
 		.SetShader(shader)
@@ -44,12 +43,12 @@ int main() {
 	// Half will have image with alpha pixels
 	const std::size_t numEntities = 10;
 	for (int i = 0; i < numEntities; ++i) {
-		float depth = (float)(rand() % 100) / 100.0f;
+		float depth = (float)(std::rand() % 100) / 100.0f;
 
 		int separation = 20;
 		int offset = 100;
-		Entity& image = state.AddEntity<Smasher::Entity>();
-		image.AddComponent<DrawableComponent>()
+		Smasher::Entity& image = state.AddEntity<Smasher::Entity>();
+		image.AddComponent<Smasher::DrawableComponent>()
 			.SetPosition(sf::Vector2f((float)(i * separation + offset), (float)(i * separation + offset)))
 			.SetScale(sf::Vector2f(100.0f, 100.0f))
 			.SetShader(shader)
@@ -57,19 +56,19 @@ int main() {
 			.SetDepth(depth);
 
 		if (i % 2 == 0) {
-			image.GetComponent<DrawableComponent>()
+			image.GetComponent<Smasher::DrawableComponent>()
 				.SetTextureAsset<Smasher::Resources::Textures::alpha_test>();
 		}
 		else {
-			image.GetComponent<DrawableComponent>()
+			image.GetComponent<Smasher::DrawableComponent>()
 				.SetTextureAsset<Smasher::Resources::Textures::small_art>();
 		}
 
 	}
 
 	// Create a window
-	Entity& window1 = state.AddEntity<Smasher::Entity>();
-	window1.AddComponent<DrawableComponent>()
+	Smasher::Entity& window1 = state.AddEntity<Smasher::Entity>();
+	window1.AddComponent<Smasher::DrawableComponent>()
 		.SetPosition(sf::Vector2f(400, 200))
 		.SetScale(sf::Vector2f(150.0f, 150.0f))
 		.SetShader(shader)
@@ -78,8 +77,8 @@ int main() {
 		.SetClipRotation(Smasher::Degrees{ 30 })
 		.SetDepth(0.6);
 
-	Entity& window2 = state.AddEntity<Smasher::Entity>();
-	window2.AddComponent<DrawableComponent>()
+	Smasher::Entity& window2 = state.AddEntity<Smasher::Entity>();
+	window2.AddComponent<Smasher::DrawableComponent>()
 		.SetPosition(sf::Vector2f(420, 220))
 		.SetScale(sf::Vector2f(150.0f, 150.0f))
 		.SetShader(shader)
@@ -87,8 +86,8 @@ int main() {
 		//.SetClipRect(sf::IntRect{ 0, 0, 64, 64 })
 		.SetDepth(0.5);
 
-	Entity& inWindowEntity = state.AddEntity<Smasher::Entity>();
-	inWindowEntity.AddComponent<DrawableComponent>()
+	Smasher::Entity& inWindowEntity = state.AddEntity<Smasher::Entity>();
+	inWindowEntity.AddComponent<Smasher::DrawableComponent>()
 		.SetPosition(sf::Vector2f(410, 210))
 		.SetScale(sf::Vector2f(100.0f, 100.0f))
 		.SetShader(shader)
@@ -98,8 +97,8 @@ int main() {
 
 
 
-	Entity& message = state.AddEntity<Smasher::Entity>();
-	message.AddComponent<TextComponent>()
+	Smasher::Entity& message = state.AddEntity<Smasher::Entity>();
+	message.AddComponent<Smasher::TextComponent>()
 		.SetString("Hello World")
 		.UseDefaults()
 		.SetFontAsset<Smasher::Resources::Fonts::arial>();
