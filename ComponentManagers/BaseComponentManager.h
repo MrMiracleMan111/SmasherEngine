e0f100bc8d8805feff8721f80f78c372bc028a34
Fleshy <Mr.MiracleMan111@gmail.com>
2025-09-06 19:34:14 -0400

    Moved BaseComponentManager definitions to "inl" file

--- ComponentManagers/BaseComponentManager.h
+++ ComponentManagers/BaseComponentManager.h
@@ -13,14 +13,12 @@ namespace Smasher {
 	* 
 	*/
 	template <class T>
-	class SMASHER_API BaseComponentManager : public IComponentManager {
+	class BaseComponentManager : public IComponentManager {
 	static_assert(std::is_base_of_v<IComponent, T>, "T should be derived from IComponent");
 
 		friend class Entity;
 	public:
-		BaseComponentManager(GameState& state) : IComponentManager(state) {
-			m_ComponentsToRemove.reserve(64); // Arbitrary can be improved upon later
-		};
+		BaseComponentManager(GameState& state);
 		BaseComponentManager() = delete;
 		BaseComponentManager(const BaseComponentManager&) = delete;
 		BaseComponentManager(BaseComponentManager&&) = delete;
@@ -33,53 +31,24 @@ namespace Smasher {
 		virtual void RenderComponents(sf::RenderWindow& rWindow) {};
 
 		template<typename... Args>
-		T& AddComponent(Entity& rEntity, Args&&... args) {
-			std::size_t index = m_Components.size();
-			auto itr = m_Components.emplace(std::forward<Args>(args)...);
-			T& rComponent = *itr;
-			SetComponentStatus(rComponent, ComponentStatus::VALID);
-			SetComponentEntity(rComponent, rEntity);
-			SetComponentManager(rComponent, *this);
-			SetComponentIterator<T>(rComponent, new typename plf::colony<T>::iterator(itr));
-			CallOnAddComponent(rComponent); // called when component has been initialiazed and added
+		T& AddComponent(Entity& rEntity, Args&&... args);
 
-			return rComponent;
-		}
-
-		void RemoveComponent(IComponent& rComponentInterface) {
-			T& rComponent = static_cast<T&>(rComponentInterface);
-			if (rComponent.GetStatus() != ComponentStatus::VALID) {
-				return;
-			}
-			typename plf::colony<T>::iterator* pItr = GetComponentIterator<T>(rComponent);
-			SetComponentStatus(rComponent, ComponentStatus::INVALID);
-			m_ComponentsToRemove.emplace_back(pItr);
-		}
+		void RemoveComponent(IComponent& rComponentInterface);
 
 	protected:
 		plf::colony<T> m_Components;
+
 	private:
-		void RemoveMarkedComponents() {
-			for (auto& itr : m_ComponentsToRemove) {
-				typename plf::colony<T>::iterator* pCompItr = itr;
-				m_Components.erase(*pCompItr);
-				delete pCompItr;
-			}
-			m_ComponentsToRemove.clear();
-		}
+		void RemoveMarkedComponents();
 
-		void PreUpdate(Millisecond delta) override {
-			RemoveMarkedComponents();
-		}
+		void PreUpdate(Millisecond delta);
 
-		void Update(Millisecond delta) override {
-			UpdateComponents(delta);
-		}
+		void Update(Millisecond delta);
 
-		void Render(sf::RenderWindow& rWindow) override {
-			RenderComponents(rWindow);
-		}
+		void Render(sf::RenderWindow& rWindow);
 
 		std::vector<typename plf::colony<T>::iterator*> m_ComponentsToRemove; // Components to be removed
 	};
 }
+
+#include "ComponentManagers/BaseComponentManager.inl"
