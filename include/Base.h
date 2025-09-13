603447eb2820cdef27e74b988fa0170f690cd4e3
Fleshy <Mr.MiracleMan111@gmail.com>
2025-09-13 10:25:33 -0400

    Added Serialization feature for components

--- include/Base.h
+++ include/Base.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <chrono>
+#include <sstream>
 #include <filesystem>
 #include <SFML/System.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
@@ -39,6 +40,58 @@ namespace Smasher {
 	using ResourcePath = std::filesystem::path;
 	using Degrees = float;
 	using Radians = float;
+
+	// Used by "ISerializeable" as a helper class to manage stream containing serialized class
+	class OutputArchive {
+	public:
+		explicit OutputArchive(std::ostream& os) : _os(os) {}
+
+		// Writes "value" to stream as a string (ex. integer 255 is written as "255" not byte 0xff
+		template<class T>
+		void Write(T& value) {
+			_os << value;
+		}
+
+		// Reinterprets value as raw bytes and writes those bytes to the stream
+		template<class T>
+		void WriteBytes(T& value) {
+			_os.write(reinterpret_cast<const char*>(&value), sizeof(T));
+		}
+
+		// Reinterprets value as raw bytes and writes those bytes to the stream
+		void WriteBytes(const char* ptr, std::size_t count) {
+			_os.write(ptr, count);
+		}
+
+	private:
+		std::ostream& _os;
+	};
+
+	// Used by "ISerializeable" as a helper class to manage stream containing serialized class
+	class InputArchive {
+	public:
+		explicit InputArchive(std::istream& is) : _is(is) {}
+
+		template<class T>
+		void Read(T& value) {
+			_is >> value;
+		}
+
+		template<class T>
+		void ReadBytes(T& value) {
+			_is.read(reinterpret_cast<char*>(&value), sizeof(T));
+		}
+
+	private:
+		std::istream& _is;
+	};
+
+	// Serializeable Interface
+	class ISerializeable {
+	public:
+		virtual void Serialize(OutputArchive& out) const = 0;
+		virtual void Deserialize(InputArchive& in) = 0;
+	};
 
 #ifdef BENCHMARK
 	namespace InternalTimers {
