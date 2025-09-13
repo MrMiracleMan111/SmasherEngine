f56c905d80c5ac82196ea03098a7e549b1ea3d9e
Fleshy <Mr.MiracleMan111@gmail.com>
2025-09-13 01:52:07 -0400

    Added Doxygen docs for DrawableComponent

--- Components/DrawableComponent.h
+++ Components/DrawableComponent.h
@@ -11,6 +11,39 @@ namespace Smasher {
 	class DrawableComponentManager;
 	class IComponent;
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+/// @brief Component meant for drawing textures to screen. Textures can be passed to the @ref SetTextureAsset
+/// as either Manifest objects or by using the @ref ResourceID of the texture.
+/// 
+/// @details
+/// The @ref DrawableComponent stores the transforms of the image and texture transforms. These
+/// transforms are passed into the base shaders use by @ref DrawableComponentManager. The image
+/// transform is stored internally for cache locality (~100x speedup in comparison to
+/// having a separate component store transform data).
+/// 
+/// The @ref SetTextureAsset method is used to specify the texture to render. For transulscent texture
+/// (ex. a window pane) the @ref TextureOptions::transluscent should be set to `true` like so:
+/// 
+/// @code
+/// GetEngine().GetResourceManager().GetOrLoadResource<Resources::Textures::Potrait, TextureResource>();
+/// Entity& image = state.AddEntity<Entity>();
+/// image.AddComponent<DrawableComponent>()
+///			.SetPosition(sf::Vector2f(100.0f, 50.0f))
+/// 		.SetScale(sf::Vector2f(20.0f, 20.0f))
+///			.SetTextureAsset<Resources::Textures::Portrait>({.transluscent = true});
+/// @endcode
+/// 
+/// In this example, the texture is loaded using the filepath instead of the Manifest Object.
+/// @code
+/// state.GetEngine().GetResourceManager().GetOrLoadResource(ResourceID { 10 }, ResourcePath{ "Resources/Textures/Portrait.png" });
+/// Entity& image = state.AddEntity<Entity>();
+/// image.AddComponent<DrawableComponent>()
+///			.SetPosition(sf::Vector2f(100.0f, 50.0f))
+/// 		.SetScale(sf::Vector2f(20.0f, 20.0f))
+///			.SetTextureAsset(ResourceID { 10 }, {.transluscent = true});
+/// @endcode
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
 	class SMASHER_API DrawableComponent : public IComponent, public Transform2DWrapper<DrawableComponent> {
 		friend class DrawableComponentManager;
 
