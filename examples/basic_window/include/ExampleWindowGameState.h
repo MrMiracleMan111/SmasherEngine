4733fcc691735abcc23540f53b140b10328f69f3
Fleshy <Mr.MiracleMan111@gmail.com>
2025-09-27 11:11:38 -0400

    Added keyboard event printing to Basic Window Example

--- examples/basic_window/include/ExampleWindowGameState.h
+++ examples/basic_window/include/ExampleWindowGameState.h
@@ -1,9 +1,16 @@
 #pragma once
+#include "EventManager.h"
 #include "GameState.h"
 
 class ExampleWindowGameState : public Smasher::GameState {
 public:
 	ExampleWindowGameState(Smasher::Engine& engine) : Smasher::GameState(engine) {};
+	~ExampleWindowGameState();
+	void Init() override;
 	void Reset() override;
 	void Render(sf::RenderWindow& window) override;
+
+private:
+	void OnKeyPress(const Smasher::Events::KeyboardEvent& e);
+	Smasher::EventSubscriptionHandle m_KeyPressSubscription;
 };
--- examples/basic_window/src/ExampleWindowGameState.cpp
+++ examples/basic_window/src/ExampleWindowGameState.cpp
@@ -1,9 +1,34 @@
 #include "ExampleWindowGameState.h"
 
+ExampleWindowGameState::~ExampleWindowGameState()
+{
+	GetEngine().GetEventManager().Unsubscribe(m_KeyPressSubscription);
+}
+
+void ExampleWindowGameState::Init() {
+	m_KeyPressSubscription = GetEngine().GetEventManager().Subscribe<Smasher::Events::KeyboardEvent>(&ExampleWindowGameState::OnKeyPress, this);
+}
+
 void ExampleWindowGameState::Reset() {
 
 }
 
 void ExampleWindowGameState::Render(sf::RenderWindow& window) {
 
+}
+
+void ExampleWindowGameState::OnKeyPress(const Smasher::Events::KeyboardEvent& e) {
+	std::string type = "NA";
+	switch (e.Type) {
+		case Smasher::Keyboard::KeyboardEventType::KEY_PRESS:
+			type = "KEY PRESS";
+		break;
+		case Smasher::Keyboard::KeyboardEventType::KEY_RELEASE:
+			type = "KEY RELEASE";
+		break;
+		case Smasher::Keyboard::KeyboardEventType::KEY_HOLD:
+			type = "KEY HOLD";
+		break;
+	}
+	std::cout << "Key Event: " << type << " Key Code: " << e.KeyCode << std::endl;
 }
