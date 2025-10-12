09bd1a29eca56ea04cfb9aaa412462cf7f597f55
Fleshy <Mr.MiracleMan111@gmail.com>
2025-10-12 12:57:20 -0400

    Switch to type_index for event subscriptions
    Instead of using a EventType enum for indexing event subscriptions, the type of the Event derived class will be used. This will allow users to add their own events without modifying Library headers.

--- include/Events.h
+++ include/Events.h
@@ -1,13 +1,10 @@
 #pragma once
 #include <chrono>
 #include <iostream>
+#include <typeindex>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include "Base.h"
-
-#define SMASHER_EVENT_TYPE(type) \
-static constexpr EventType GetStaticEventType() { return type; }; \
-EventType GetEventType() const override { return GetStaticEventType(); }; \
 
 namespace Smasher {
 	using SMASHER_TIMESTAMP = std::chrono::time_point<std::chrono::system_clock>;
@@ -15,7 +12,8 @@ namespace Smasher {
 	struct SMASHER_API Event {
 		friend class EventManager;
 		SMASHER_TIMESTAMP Timestamp;
-		virtual EventType GetEventType() const = 0;
+		std::type_index GetEventType() const { return _GetEventType(); }
+
 		virtual ~Event() {}
 		Event() = delete;
 
@@ -24,12 +22,17 @@ namespace Smasher {
 	protected:
 		Event(SMASHER_TIMESTAMP timestamp) : Timestamp(timestamp) {}
 		bool Propagate = true; // When set to false, event subscriptions will stop handling this event
+	
+	private:
+		// Retrieve derived Event type
+		virtual std::type_index _GetEventType() const final {
+			return std::type_index(typeid(*this));
+		}
 	};
 
 	namespace Events {	
 		struct SMASHER_API DummyEvent : public Event {
 			const char *Message;
-			SMASHER_EVENT_TYPE(EventType::DummyEvent)
 			DummyEvent() = delete;
 			DummyEvent(SMASHER_TIMESTAMP timestamp, const char *message) :
 				Event(timestamp), Message(message) {
@@ -38,7 +41,6 @@ namespace Smasher {
 
 		struct SMASHER_API DummyEventExtra : public Event {
 			const char* Message;
-			SMASHER_EVENT_TYPE(EventType::DummyEventExtra)
 			DummyEventExtra() = delete;
 			DummyEventExtra(SMASHER_TIMESTAMP timestamp, const char* message) :
 				Event(timestamp), Message(message) {
@@ -48,7 +50,6 @@ namespace Smasher {
 		struct SMASHER_API KeyboardEvent : public Event {
 			Smasher::Keyboard::KeyboardEventType Type;
 			sf::Keyboard::Key KeyCode;
-			SMASHER_EVENT_TYPE(EventType::KeyboardEvent)
 			KeyboardEvent() = delete;
 			KeyboardEvent(SMASHER_TIMESTAMP timestamp, Keyboard::KeyboardEventType type, sf::Keyboard::Key keycode) :
 				Event(timestamp), Type(type), KeyCode(keycode) {}
@@ -57,7 +58,6 @@ namespace Smasher {
 		// Intermediate class to represent mouse events
 		struct MouseEvent : public Event {
 			Mouse::MouseEventType Type;
-			virtual EventType GetEventType() const = 0;
 			~MouseEvent() {}
 		protected:
 			MouseEvent() = delete;
@@ -68,7 +68,6 @@ namespace Smasher {
 		struct SMASHER_API MouseButtonEvent : public MouseEvent {
 			sf::Mouse::Button ButtonCode;
 			sf::Vector2i Position;
-			SMASHER_EVENT_TYPE(EventType::MouseButtonEvent)
 			MouseButtonEvent(SMASHER_TIMESTAMP timestamp, Mouse::MouseEventType type, sf::Mouse::Button buttoncode, sf::Vector2i position) :
 				MouseEvent(timestamp, type), ButtonCode(buttoncode), Position(position) {}
 		};
@@ -76,7 +75,6 @@ namespace Smasher {
 		struct SMASHER_API MouseScrollWheelEvent : public MouseEvent {
 			float Delta;
 			sf::Vector2i Position;
-			SMASHER_EVENT_TYPE(EventType::MouseScrollWheelEvent)
 			MouseScrollWheelEvent(SMASHER_TIMESTAMP timestamp, Mouse::MouseEventType type, float delta, sf::Vector2i position) :
 				MouseEvent(timestamp, type), Delta(delta), Position(position) {}
 		};
@@ -84,19 +82,16 @@ namespace Smasher {
 		struct SMASHER_API MouseMoveEvent : public MouseEvent {
 			sf::Vector2i Delta;
 			sf::Vector2i Position;
-			SMASHER_EVENT_TYPE(EventType::MouseMoveEvent)
 			MouseMoveEvent(SMASHER_TIMESTAMP timestamp, Mouse::MouseEventType type, sf::Vector2i delta, sf::Vector2i position) :
 				MouseEvent(timestamp, type), Delta(delta), Position(position) {}
 		};
 
 		struct SMASHER_API WindowCloseEvent : public Event {
-			SMASHER_EVENT_TYPE(EventType::WindowCloseEvent)
 			WindowCloseEvent(SMASHER_TIMESTAMP timestamp) : Event(timestamp) {}
 		};
 
 		struct SMASHER_API WindowResizeEvent : public Event {
 			sf::Vector2i WindowSize;
-			SMASHER_EVENT_TYPE(EventType::WindowResizeEvent)
 				WindowResizeEvent(SMASHER_TIMESTAMP timestamp, sf::Vector2i size) :
 				Event(timestamp), WindowSize(size) {}
 		};
