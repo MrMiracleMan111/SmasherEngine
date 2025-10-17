7e5a728774e068ad8aa4439524416bd7f54c1266
Fleshy <Mr.MiracleMan111@gmail.com>
2025-10-17 17:03:52 -0400

    Removed backslash filepath

--- examples/basic_serialize/main.cpp
+++ examples/basic_serialize/main.cpp
@@ -2,7 +2,7 @@
 #include "SavedComponent.h"
 #include "SerializeLayer.h"
 #include "Manifest.h"
-#include "ComponentManagers\DrawableComponentManager.h"
+#include "ComponentManagers/DrawableComponentManager.h"
 
 int main() {
 	Smasher::Engine engine(640, 420);
