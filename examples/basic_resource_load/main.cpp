eed76771dc710a8030f8e41396a5f49efdd5bfa2
Fleshy <Mr.MiracleMan111@gmail.com>
2025-11-14 20:34:19 -0500

    Added texture clip rect test to basic_resource_load example

--- examples/basic_resource_load/main.cpp
+++ examples/basic_resource_load/main.cpp
@@ -9,14 +9,21 @@
 #include "Smasher/Components/DrawableComponent.h"
 
 int main() {
+	float angle = 0.f;
 	Smasher::Engine engine(640, 420);
 	engine.GetResourceManager().SetResourceDirectory(Smasher::Manifest::Metadata::RESOURCES_DIRECTORY);
 
 	ExampleResourcesLayer& exampleLayer = engine.PushLayer<ExampleResourcesLayer>();
+	
+	Smasher::DrawableComponentManager& drawableMgr = static_cast<Smasher::DrawableComponentManager&>(exampleLayer.GetComponentManager<Smasher::DrawableComponent>());
+	std::shared_ptr<Smasher::ShaderResource> shader = engine.GetResourceManager().GetOrLoadResource<Smasher::Manifest::Shaders::basic_texture_shader, Smasher::ShaderResource>();
+	sf::Glsl::Mat4 viewProjectionMatrix = sf::Glsl::Mat4(engine.GetWindow().getView().getTransform().getMatrix());
+	shader->GetShader().setUniform("ViewProjectionMatrix", viewProjectionMatrix);
+	drawableMgr.SetShaderResource(shader);
 
 	Smasher::Entity& image = exampleLayer.AddEntity<Smasher::Entity>();
 	image.AddComponent<Smasher::DrawableComponent>()
-		.SetPosition(sf::Vector2f(500.0f, 300.0f))
+		.SetPosition(sf::Vector2f(600.0f, 300.0f))
 		.SetScale(sf::Vector2f(400.0f, 400.0f))
 		.SetTextureAsset<Smasher::Manifest::Textures::small_art>({}) // Default texture options
 		//.SetClipRect(sf::IntRect{ 0, 0, 64, 64 })
@@ -25,17 +32,25 @@ int main() {
 
 	Smasher::Entity& clipTest = exampleLayer.AddEntity<Smasher::Entity>();
 	clipTest.AddComponent<Smasher::DrawableComponent>()
-		.SetPosition(sf::Vector2f(50.0f, 200.0f))
-		.SetScale(sf::Vector2f(100.0f, 75.0f))
+		.SetPosition(sf::Vector2f(180.0f, 330.0f))
+		.SetScale(sf::Vector2f(200.f, 150.f))
+		.SetScale(sf::Vector2f(200.f, 300.f))
 		.SetTextureAsset<Smasher::Manifest::Textures::clip_test>({}) // Default texture options
 		.SetClipRect(sf::IntRect{ 0, 0, 24, 18 })
-		//.SetClipRotation(Smasher::Degrees{ 30 })
+		.SetClipRect(sf::IntRect{ 0, 18, 24, 36 })
+		.SetClipRotation(Smasher::Degrees{ 45 })
 		.SetDepth(0.1f);
+
+	Smasher::EventSubscriptionHandle m_MouseMoveHandle = exampleLayer.Subscribe<Smasher::Events::MouseMoveEvent>([&angle, &clipTest](Smasher::Events::MouseMoveEvent& event) {
+		angle += 1.f;
+		clipTest.GetComponent<Smasher::DrawableComponent>()
+			.SetClipRotation(Smasher::Degrees{ angle });
+	});
 
 	// Create drawable component instance without texture
 	Smasher::Entity& shape = exampleLayer.AddEntity<Smasher::Entity>();
 	shape.AddComponent<Smasher::DrawableComponent>()
-		.SetPosition(sf::Vector2f(50.0f, 300.0f))
+		.SetPosition(sf::Vector2f(400.0f, 50.0f))
 		.SetScale(sf::Vector2f(100.0f, 100.0f))
 		//.SetClipRect(sf::IntRect{ 0, 0, 64, 64 })
 		.SetColor(sf::Color::Magenta)
@@ -53,7 +68,7 @@ int main() {
 		int offset = 100;
 		Smasher::Entity& image = exampleLayer.AddEntity<Smasher::Entity>();
 		image.AddComponent<Smasher::DrawableComponent>()
-			.SetPosition(sf::Vector2f((float)(i * separation + offset), (float)(i * separation + offset)))
+			.SetPosition(sf::Vector2f((float)(i * separation + offset), (float)(i * 0.3f * separation + offset)))
 			.SetScale(sf::Vector2f(100.0f, 100.0f))
 			.SetDepth(depth);
 
