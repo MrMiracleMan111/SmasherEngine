7d4cdd1b4f801c5ebb47f22d659e8f7ce9c08edb
Fleshy <Mr.MiracleMan111@gmail.com>
2026-02-15 10:30:42 -0500

    Moved ErrorCodes.h location

---
+++ include/Smasher/ErrorCodes.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "Smasher/Base.h"
+
+// ==== NO ERROR ====
+#define ERROR_NoError 0
+
+// ==== LAYER EXCEPTIONS ====
+#define ERROR_LayerNotFound 100
+#define ERROR_LayerDuplicate 101
+#define ERROR_LayerEntityNotFound 102
+#define ERROR_CannotRemoveBaseLayer 103
+
+// ==== COMPONENT EXCEPTIONS ====
+#define ERROR_ComponentInvalid 200
+#define ERROR_ComponentDowncastFailed 202
+#define ERROR_MissingComponentDependency 203
+
+// ==== CAMERA EXCEPTIONS ====
+#define ERROR_CameraTargetNotSet 300
+
+// ==== ENTITY EXCEPTIONS ====
+#define ERROR_EntityDuplicateComponent 400
+#define ERROR_EntityComponentNotFound 401
+
+// ==== EVENT EXCEPTIONS ====
+#define ERROR_EventHandleInvalid 500
+
+// ==== RESOURCE EXCEPTIONS ====
+#define ERROR_ResourceFailedToLoad 600
+#define ERROR_ResourceNotLoaded 601
+#define ERROR_ResourceAlreadyExists 602
+#define ERROR_ResourceInvalidNumPaths 603
+
+// ==== PHYSICS EXCEPTIONS ====
+#define ERROR_Box2DWorldAlreadyCreated 700
+#define ERROR_Box2DBodyIdInvalid 701
+
+// ==== OPENGL EXCEPTIONS ====
+#define ERROR_GLEWInitFailed 800
+
+// ==== JOBS EXCEPTIONS ====
+#define ERROR_NotEnoughThreads 900
+#define ERROR_NoJobsAvailable 901
+#define ERROR_JobHasNoParents 902
