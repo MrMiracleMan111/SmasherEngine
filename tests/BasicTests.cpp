e8abdc2f855c2fa6328731c09a8b731702aea6d1
Fleshy <Mr.MiracleMan111@gmail.com>
2026-02-16 16:09:05 -0500

    Comment fix in basic tests

--- tests/BasicTests.cpp
+++ tests/BasicTests.cpp
@@ -435,7 +435,7 @@ TEST(JobManagerTest, JobManagerTickProducer) {
 	});
 
 	layer.Activate();
-	engine.Run(); // Engine should shutdown after third update
+	engine.Run(); // Engine should shutdown after the third update
 
 	EXPECT_EQ(3, triggered_count);
 }
