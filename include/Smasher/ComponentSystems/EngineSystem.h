a3a273ed78b9560374a541255823c763f3cf18c5
Fleshy <Mr.MiracleMan111@gmail.com>
2026-02-28 09:13:26 -0500

    Added EngineReference component
    This is a singleton component that stores a reference to the engine that this registry belongs to

---
+++ include/Smasher/ComponentSystems/EngineSystem.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Smasher/Base.h"
+
+namespace Smasher {
+	class Engine;
+	namespace EngineSystem {
+		struct Context {
+			std::reference_wrapper<Engine> m_EngineRef;
+		};
+	}
+}
