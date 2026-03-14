fc41b18da125917b2d1385435337448903112f65
Fleshy <Mr.MiracleMan111@gmail.com>
2026-03-14 18:44:46 -0400

    Added SMASHER_API export to EngineSystem::Context

--- include/Smasher/ComponentSystems/EngineSystem.h
+++ include/Smasher/ComponentSystems/EngineSystem.h
@@ -4,8 +4,11 @@
 namespace Smasher {
 	class Engine;
 	namespace EngineSystem {
-		struct Context {
+		struct SMASHER_API Context {
 			std::reference_wrapper<Engine> engineRef;
 		};
+
+		SMASHER_API ErrorCode ClearWindow(entt::registry& registry);
+		SMASHER_API ErrorCode DisplayWindow(entt::registry &registry);
 	}
 }
